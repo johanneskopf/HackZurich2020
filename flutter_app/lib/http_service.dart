02ab17c662275cb8b8f503b5f3e626eb65e904c3
johanneskopf <johanneskopf@gmx.at>
2020-09-18 22:12:48 +0200

    add rest api access example

---
+++ flutter_app/lib/http_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:flutter_app/posts_model.dart';
+import 'package:http/http.dart';
+
+class HttpService {
+  final String postsURL = "https://jsonplaceholder.typicode.com/posts";
+
+  Future<List<Post>> getPosts() async {
+    Response res = await get(postsURL);
+
+    if (res.statusCode == 200) {
+      List<dynamic> body = jsonDecode(res.body);
+
+      List<Post> posts = body
+          .map(
+            (dynamic item) => Post.fromJson(item),
+      )
+          .toList();
+
+      return posts;
+    } else {
+      throw "Can't get posts.";
+    }
+  }
+}
---
+++ flutter_app/lib/posts.dart
@@ -0,0 +1,44 @@
+import 'package:flutter_app/http_service.dart';
+import 'package:flutter_app/posts_detail.dart';
+import 'package:flutter_app/posts_model.dart';
+import 'package:flutter/material.dart';
+
+class PostsPage extends StatelessWidget {
+  final HttpService httpService = HttpService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Posts"),
+      ),
+      body: FutureBuilder(
+        future: httpService.getPosts(),
+        builder: (BuildContext context, AsyncSnapshot<List<Post>> snapshot) {
+          if (snapshot.hasData) {
+            List<Post> posts = snapshot.data;
+            return ListView(
+              children: posts
+                  .map(
+                    (Post post) => ListTile(
+                  title: Text(post.title),
+                  subtitle: Text("${post.userId}"),
+                  onTap: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => PostDetail(
+                        post: post,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+                  .toList(),
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
+}
