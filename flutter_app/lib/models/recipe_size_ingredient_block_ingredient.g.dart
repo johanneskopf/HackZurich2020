f5be5852d52e96978d0b50147b2ed0b03b80487c
johanneskopf <johanneskopf@gmx.at>
2020-09-19 23:05:50 +0200

    fix recipe ingredients amount

--- flutter_app/lib/models/recipe_size_ingredient_block_ingredient.g.dart
+++ flutter_app/lib/models/recipe_size_ingredient_block_ingredient.g.dart
@@ -8,19 +8,20 @@ part of 'recipe_size_ingredient_block_ingredient.dart';
 
 RecipeSizeIngredientBlockIngredient
     _$RecipeSizeIngredientBlockIngredientFromJson(Map<String, dynamic> json) {
-  double _quantity = 0.0;
-  RegExp exp = new RegExp(r"quantity=[0-9]+(\.[0-9]*)");
-  Iterable<RegExpMatch> matches = exp.allMatches(json['amount']);
-  if(matches.isNotEmpty)
-    _quantity = num.tryParse(matches.first.group(0).replaceFirst("quantity=", ""))?.toDouble();
   return RecipeSizeIngredientBlockIngredient()
     ..id = json['id'] as String
     ..isMainIngredient = json['is_main_ingredient'] as bool
     ..text = json['text'] as String
-    ..amount = json['amount'] as String
-    ..quantity = _quantity
-    ..availability = json['availability']
-    ..cultivar = json['cultivar'] as String;
+    ..amount = json['amount'] == null
+        ? null
+        : RecipeIngredientAmount.fromJson(
+            json['amount'] as Map<String, dynamic>)
+    ..availability = json['availability'] == null
+        ? null
+        : Availability.fromJson(json['availability'] as Map<String, dynamic>)
+    ..cultivar = json['cultivar'] == null
+        ? null
+        : Cultivar.fromJson(json['cultivar'] as Map<String, dynamic>);
 }
 
 Map<String, dynamic> _$RecipeSizeIngredientBlockIngredientToJson(
@@ -32,4 +33,27 @@ Map<String, dynamic> _$RecipeSizeIngredientBlockIngredientToJson(
       'amount': instance.amount,
       'availability': instance.availability,
       'cultivar': instance.cultivar,
+    };
+
+Availability _$AvailabilityFromJson(Map<String, dynamic> json) {
+  return Availability()
+    ..key = json['key'] as String
+    ..name = json['name'] as String;
+}
+
+Map<String, dynamic> _$AvailabilityToJson(Availability instance) =>
+    <String, dynamic>{
+      'key': instance.key,
+      'name': instance.name,
+    };
+
+Cultivar _$CultivarFromJson(Map<String, dynamic> json) {
+  return Cultivar()
+    ..id = json['id'] as String
+    ..name = json['name'] as String;
+}
+
+Map<String, dynamic> _$CultivarToJson(Cultivar instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
     };
